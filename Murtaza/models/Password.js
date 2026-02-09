f1bd9335e12017af411d8507177754edcba67105
Murtaza <murtazafetrat0012@gmail.com>
2026-02-09 16:48:21 +0100

    creation de model Password

---
+++ Murtaza/models/Password.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const passwordSchem = new mongoose.Schema({
+    userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User ID requis'],
+    index: true
+  },
+  titre: {
+    type: String,
+    required: true,
+    max: 30
+  },
+  site: {
+    type: String,
+    required,
+    max: 255
+  },
+  encryptedPassword: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    max: 45,
+  },
+  userName: {
+    type: String,
+    max: 30
+  },
+    category: {
+    type: String,
+    enum: {
+      values: ['social', 'banking', 'email', 'shopping', 'work', 'other'],
+      message: 'Catégorie invalide'
+    },
+    default: 'other'
+  }
+
+})
+
+const Password = mongoose.model('Password', passwordSchem);
+export default Password;
