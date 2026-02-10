957097921d6b2ee2b6f43a08ffbe06d97c370ba7
Murtaza <murtazafetrat0012@gmail.com>
2026-02-10 14:33:31 +0100

    adding le routage d'authentication dans le server.js

--- Murtaza/server.js
+++ Murtaza/server.js
@@ -1,9 +1,25 @@
 import express from 'express';
+import connectionDB from './config/db';
+import routeLoginRegister from './routes/authRoutes.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-
+await connectionDB();
 const app = express();
-app.use(express.json())
 
+// ecoute a front s'il envoi les requets
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+
+app.use(routeLoginRegister)
+
+// page d'accueil
 app.get('/', async(req, res) => {
     try{
         console.log("C'est le page d'accuiel");
@@ -13,6 +29,8 @@ app.get('/', async(req, res) => {
     }
 });
 
+
+// page de enrigistrer d'un utilisateur ou form d'enrigistrement
 app.get('/register', async(req, res) => {
     try{
         console.log(`C'est le page de register pour afficher les donnees`);
@@ -21,15 +39,9 @@ app.get('/register', async(req, res) => {
     }
 })
 
-app.post('/register', async(req, res) => {
-    try{
-        // ici on requpere la donnee de req.body
-        console.log("cest le page de resgister pour poster des donnees")
-    }catch(error){
-        console.log(error);
-    }
-})
 
+
+// page de login
 app.get('/login', async(req, res) => {
     try{
         console.log("Page de login pour afficher les donnees")
@@ -38,14 +50,8 @@ app.get('/login', async(req, res) => {
     }
 });
 
-app.post('/login', async(req, res) => {
-    try{
-        console.log("Page de login pour requperer les donnees de login");
-    }catch(error){
-        console.log(error);
-    }
-})
 
+// page de dashboard
 app.get('/dashboard', async(req, res) => {
     try{
         console.log("Page de dashboard");
@@ -54,6 +60,7 @@ app.get('/dashboard', async(req, res) => {
     }
 });
 
+// route de loging out
 app.get('/logout', async(req, res) => {
     try{
         console.log("Page de logout");
@@ -61,6 +68,7 @@ app.get('/logout', async(req, res) => {
         console.log(error)
     }
 });
+
 
 const PORT = 3000;
 
